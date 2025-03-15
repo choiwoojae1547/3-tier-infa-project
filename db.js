2b37b784ac0355258f4e98a1a072ba969913b901
Choi Woo Jae <cwj1547@gmail.com>
2025-03-15 19:15:41 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,28 +1,19 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-// MariaDB 연결 설정
-const db = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'localhost',
     user: 'admin',
     password: 'pwadmin',
-    database: 'mydatabase'
+    database: 'mydatabase',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-// 연결
-db.connect((err) => {
-    if (err) {
-        console.error('데이터베이스 연결 실패: ' + err.stack);
-        return;
-    }
-    console.log('데이터베이스 연결 성공');
-});
-
-// 데이터베이스 쿼리 실행 예시
 function getUserData(userId, callback) {
-    const query = 'SELECT * FROM users WHERE id = ?';
-    db.query(query, [userId], (err, results) => {
+    pool.query('SELECT * FROM users WHERE id = ?', [userId], (err, results) => {
         if (err) {
-            console.error('쿼리 실행 실패: ' + err.stack);
+            console.error('쿼리 실행 실패:', err);
             return callback(err, null);
         }
         callback(null, results);
