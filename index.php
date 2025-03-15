da8108fbcf00dac8e8a707fe4e2f597089461a10
Choi Woo Jae <cwj1547@gmail.com>
2025-03-15 19:14:49 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
-<?php
-// 애플리케이션 서버 URL
-$api_url = "http://10.0.2.82:<포트>/api/endpoint";
+$api_url = "http://10.0.2.82:3000/api/endpoint";
 
-// 애플리케이션 서버로 GET 요청 보내기
-$response = file_get_contents($api_url);
-
+$response = @file_get_contents($api_url); // suppress error output
 if ($response === FALSE) {
-    echo "애플리케이션 서버로부터 응답을 받지 못했습니다.";
+    $error = error_get_last(); // 디버깅용
+    echo "애플리케이션 서버로부터 응답을 받지 못했습니다. 오류: " . $error['message'];
 } else {
-    // 애플리케이션 서버의 응답 출력
     echo "애플리케이션 서버 응답: " . $response;
 }
-?>
